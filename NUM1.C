b0ff0d130a133c1da5d2742607d08317eeaec10e
eddie80 <34468722+eddie80@users.noreply.github.com>
2018-02-26 23:31:02 +0800

    The original code
    The original code I made for Fang in 2001

---
+++ NUM1.C
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+main()
+{
+ int i,j,x;
+ time_t lt;
+
+ clrscr();
+ printf("\nPlease input a number:(1....9999)==> ");
+ scanf("%d",&x);
+ printf("\n");
+ lt=time(NULL);
+ for(i=0;i<3;i++)
+ {
+  for(j=0;j<10;j++)
+  {
+   printf(" %d",(rand()*lt%9+131)*x*971/7%10);
+   }
+  printf("  ");
+  }
+ printf("\n");
+ }
+
