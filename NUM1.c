e2c938288d3a32dc57e8cad56c30bdb429626134
eddie80 <eddie_tsai64@yahoo.com.tw>
2018-03-03 00:14:22 +0800

    To optimized the rand()

--- NUM1.c
+++ NUM1.c
@@ -6,16 +6,18 @@
 
 int main()
 {
- int i,j,k;
- int long x;
- time_t lt;
+srand(time(NULL)); 
+int i,j,k;
+int long x;
+time_t lt;
+int r=rand();
 
 system("clear");
 
  printf("\nPlease input a number:(100....9999)==> "); //To enter a number (range 100 ~ 9999) for generating the random numbers
   scanf("%ld",&x);
 // printf("\n"); 
-  lt=time(NULL);
+// lt=time(NULL);
 if ( x < 100 | x > 9999) //To limited the range of the input value from 100 to 9999
 {
  printf ("This is an invalid value\n\n"); //To show the value is invalide when it's out of the range
@@ -29,7 +31,7 @@ for (k=0;k<30;k++) //To generate 30 rows of random numbers
   {
    for(j=0;j<10;j++) //To generate 10 randow numbers in each set
    {
-    printf(" %ld",(rand()*lt%9+131)*x*971/7%10);
+    printf(" %ld",(rand()*r%9+131)*x*971/7%10);
     }
    printf("  ");
    }
