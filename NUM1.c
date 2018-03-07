2a4d29a746faee9d87c8c20cbc0b94b4fcd45f3c
eddie80 <eddie_tsai64@yahoo.com.tw>
2018-03-08 00:03:08 +0800

    To make the code more simple and clean

--- NUM1.c
+++ NUM1.c
@@ -6,38 +6,35 @@
 
 int main()
 {
-srand(time(NULL)); 
-int i,j,k;
-int long x;
-time_t lt;
-int r=rand();
+ srand(time(NULL)); 
+ int i,j,k;
+ int long x;
+ int r=rand();
 
-system("clear");
+ system("clear");
 
- printf("\nPlease input a number:(100....9999)==> "); //To enter a number (range 100 ~ 9999) for generating the random numbers
-  scanf("%ld",&x);
-// printf("\n"); 
-// lt=time(NULL);
-if ( x < 100 | x > 9999) //To limited the range of the input value from 100 to 9999
-{
- printf ("This is an invalid value\n\n"); //To show the value is invalide when it's out of the range
- }
-else
-{ 
- printf("\n");
-for (k=0;k<30;k++) //To generate 30 rows of random numbers
-{
-  for(i=0;i<3;i++) //To generate 3 sets of random numbers in one row
-  {
-   for(j=0;j<10;j++) //To generate 10 randow numbers in each set
+ printf("\nPlease input a number:(1....9)==> "); //To enter a number (range 1 ~ 9) for generating the random numbers
+ scanf("%ld", &x);
+ if ( x < 1 | x > 9) //To limited the range of the input value from 1 to 9
+ {
+  printf ("\n <<Warning>> This is an invalid value\n\n"); //To show the value is invalide when it's out of the range
+  }
+ else
+ { 
+  printf("\n");
+  for (k=0;k<30;k++) //To generate 30 rows of random numbers
+  {  
+   for(i=0;i<3;i++) //To generate 3 sets of random numbers in one row
    {
-    printf(" %ld",(rand()*r%9+131)*x*971/7%10);
+    for(j=0;j<10;j++) //To generate 10 randow numbers in each set
+    {
+     printf(" %ld",(rand()*r%9+131)*x*971/7%10);
+     }
+    printf("  ");
     }
-   printf("  ");
+   printf("\n");
    }
   printf("\n");
+  return 0;
+  }   
  }
- printf("\n");
-return 0;
- }  
-}
